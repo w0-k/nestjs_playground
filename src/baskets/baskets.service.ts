726ee305ea2fcd51c62f6fd06527f203313acc7c
Wojtek Kiezik <wojciech.kiezik@xperi.com>
2022-11-05 00:36:59 +0100

    done

--- src/baskets/baskets.service.ts
+++ src/baskets/baskets.service.ts
@@ -16,11 +16,26 @@ export class BasketsService {
             relations: ["items"]
         });
         const shopItems = await ShopItem.find();
-        debugger;
+        const basketsItemsMap = baskets.map(({ items }) => items.map(({ name }) => name));
+        
+        const basketPricesMap = basketsItemsMap.map((basket) => basket.map(
+            (name) => shopItems.find((shopItem) => shopItem.name === name).price
+        ));
 
+        let basketsTotalValue = 0;
+        const sumFn = (a: number, b: number) => a + b;
+
+        const avgPricesMap = basketPricesMap.map((basket) => {
+            const sum = basket.reduce(sumFn, 0);
+            basketsTotalValue += sum;
+            return Number((sum / basket.length).toFixed(2));
+        });
+
+        const avgItemCost = avgPricesMap.reduce(sumFn, 0) / avgPricesMap.length;
+        const toFixedNum = (value: number) => Number(value.toFixed(2));
         return {
-            avgItemInBasketCost: 0,
-            avgBasketValue: 0,
+            avgItemInBasketCost: toFixedNum(avgItemCost),
+            avgBasketValue: toFixedNum(basketsTotalValue / baskets.length),
         }
     }
 }
